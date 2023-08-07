4471fb50153129c3ea0706d79cefc9f47df5a97a
ffaaddiill <fadilah.ajiq.surya@gmail.com>
2023-08-07 17:07:18 +0700

    resolved command to start google chrome browser to be fullscreen after reload

--- restart.bat
+++ restart.bat
@@ -1,4 +1,4 @@
 @echo off
 START /B /WAIT cmd /c taskkill /F /IM chrome.exe /T > nul
-START chrome -incognito http://absenqr.test/qrs/qs
+START chrome --start-fullscreen -incognito http://absenqr.test/qrs/qs
 exit
